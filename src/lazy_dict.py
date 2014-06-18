4f4a8b082a19dc2d81641a13241118d827512252
impxia <impxia@gmail.com>
2014-06-18 17:33:49 +0800

    inital commit

---
+++ src/lazy_dict.py
@@ -0,0 +1,88 @@
+__author__ = 'shuangxia'
+
+
+def get_structure(lazy_object):
+    """Get lazy_object's structure by recursively getting attributes' key without evaluating their real value
+    Args:
+        lazy_object: object to inspect
+    Returns:
+        dict
+    """
+    structure = {}
+    for key in dir(lazy_object):
+        if not key.startswith('_'):
+            if lazy_object._is_subset(key):
+                structure[key] = get_structure(getattr(lazy_object, key))
+            else:
+                structure[key] = None
+    return structure
+
+
+def to_dict(lazy_object):
+    """Convert lazy_object to a dictionary by recursively expanding.
+    Sub-level child will still remain untouched.
+    Args:
+        lazy_object: object to convert
+    Returns:
+        dict
+    """
+    result = {}
+    for key in dir(lazy_object):
+        if not key.startswith('_'):
+            if lazy_object._is_subset(key):
+                result[key] = to_dict(getattr(lazy_object, key))
+            else:
+                result[key] = getattr(lazy_object, key)
+    return result
+
+
+class lazy_property(object):
+    """meant to be used for lazy evaluation of an object attribute.
+    property should represent non-mutable data, as it replaces itself.
+    """
+
+    def __init__(self, fget):
+        self.fget = fget
+        self.func_name = fget.__name__
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return None
+        value = self.fget(obj)
+        setattr(obj, self.func_name, value)
+        return value
+
+
+class LazyObject(object):
+    """Base class for lazy dictionary.
+    Customize dictionary should extend this class and define its own property
+    See test_lazy_dict for example
+    """
+    def __init__(self):
+        self.__subset__ = set()
+
+    def _is_subset(self, key):
+        return key in self.__subset__
+
+    def _get_structure(self):
+        return get_structure(self)
+
+    def _keys(self):
+        keys = set()
+        for key in dir(self):
+            if not key.startswith('_'):
+                keys.add(key)
+        return keys
+
+    def _to_dict(self):
+        return to_dict(self)
+
+    def __getitem__(self, index):
+        try:
+            value = getattr(self, index)
+        except:
+            value = None
+        return value
+
+    def __setitem__(self, index, value):
+        setattr(self, index, value)
---
+++ test/test_lazy_dict.py
@@ -0,0 +1,66 @@
+from src.lazy_dict import LazyObject, lazy_property
+from nose import tools
+
+
+class Book(LazyObject):
+    def __init__(self, book_id):
+        super(Book, self).__init__()
+        self.id = book_id
+        self.__subset__ = {'review'}
+
+    @lazy_property
+    def author(self):
+        return self._get_author()
+
+    def _get_author(self):
+        # read file content from book_id.json
+        info = {'author': 'author'}
+        return info.get('author')
+
+    @lazy_property
+    def content(self):
+        cmd = 'scp remote_host:/data/book/book_id.pdf /data/book/book_id/content.pdf'
+        # run cmd
+        return 'binary stream of /data/book/book_id/content.pdf'
+
+    @lazy_property
+    def review(self):
+        return Review(self.id)
+
+
+class Review(LazyObject):
+    def __init__(self, book_id):
+        super(Review, self).__init__()
+        self.id = book_id
+
+    @lazy_property
+    def count(self):
+        sql = 'select count(*) from db.review where book_id=self.id'
+        # run sql
+        return 10
+
+    @lazy_property
+    def latest(self):
+        sql = 'select * from db.review where book_id=self.id order by time desc limit 1'
+        # run sql
+        return 'Latest review'
+
+
+def test():
+    book = Book('book1 id')
+    tools.eq_(len(book.__dict__), 2, 'Book should have only 2 properties, id and _subset')
+    author = book.author
+    tools.eq_(len(book.__dict__), 3, 'Book should have 1 more property, author')
+    review = book.review
+    tools.eq_(len(book.__dict__), 4, 'Book should have 1 more property, review')
+    tools.eq_(len(book.review.__dict__), 2, 'Review should have only 2 properties, id and _subset')
+    latest = book.review.latest
+    tools.eq_(len(book.review.__dict__), 3, 'Review should have 1 more property, review')
+    structure = book._get_structure()
+    tools.eq_(len(structure), 4, 'Structure should have 4 properties.')
+    tools.eq_(structure['id'], None, 'Structure should have id with no value.')
+    # structure should not have _subset
+    tools.assert_raises(KeyError, structure.__getitem__, '__subset__')
+    data = book._to_dict()
+    tools.eq_(len(data), 4, 'Data should have 4 properties.')
+    tools.eq_(data['review']['count'], 10, 'Data should have actual data.')
